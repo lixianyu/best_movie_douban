f5ea490f2dc74113cbb8ade86592d92e1caf09a5
lixianyu <lbonus@163.com>
2018-01-31 09:48:57 +0800

    完成任务2 ， ChromeDriver是2.35(Supports Chrome v62-64)，重新安装了Chrome浏览器v64版本后，搞定。

--- DoubanCrawler.py
+++ DoubanCrawler.py
@@ -1,3 +1,4 @@
+import expanddouban
 
 gMyCategory = ['科幻', '动作', '悬疑']
 gMyLocation = ['大陆', '美国', '香港', '台湾', '日本', '韩国', '英国', '法国', '德国', '意大利', '西班牙', '印度', '泰国', '俄罗斯', '伊朗', '加拿大', '澳大利亚', '爱尔兰', '瑞典', '巴西', '丹麦']
@@ -8,6 +9,13 @@ def getMovieUrl(category, location):
     url = "https://movie.douban.com/tag/#/?sort=S&range=9,10&tags=电影," + category + "," + location
     return url
 
-for cat in gMyCategory:
-    for loc in gMyLocation:
-        print(getMovieUrl(cat, loc))
+# for test Task1
+# for cat in gMyCategory:
+#     for loc in gMyLocation:
+#         print(getMovieUrl(cat, loc))
+
+# for test Task2
+# url = "https://movie.douban.com/tag/#/?sort=S&range=9,10&tags=电影,喜剧,美国"
+# html = expanddouban.getHtml(url, True, 2.4)
+# print('html = {}\n'.format(html))
+# print('type(html) = {}'.format(type(html)))
--- expanddouban.py
+++ expanddouban.py
@@ -1,11 +1,11 @@
 from selenium import webdriver
-import time 
+import time
 
 """
 url: the douban page we will get html from
-loadmore: whether or not click load more on the bottom 
-waittime: seconds the broswer will wait after intial load and 
-""" 
+loadmore: whether or not click load more on the bottom
+waittime: seconds the broswer will wait after intial load and
+"""
 def getHtml(url, loadmore = False, waittime = 2):
     browser = webdriver.Chrome('chromedriver')
     browser.get(url)
@@ -22,7 +22,8 @@ def getHtml(url, loadmore = False, waittime = 2):
     browser.quit()
     return html
 
-# for test
-#url = "https://movie.douban.com/tag/#/?sort=S&range=9,10&tags=电影,剧情,美国"
-#html = getHtml(url)
-#print(html) 
+#for test
+# url = "https://movie.douban.com/tag/#/?sort=S&range=9,10&tags=电影,剧情,美国"
+# html = getHtml(url)
+# print(html)
+# print(type(html))
